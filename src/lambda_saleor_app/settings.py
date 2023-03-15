2746bbd1cbf15d67b138d26e12544f9d00df0254
Łukasz Kostka <lukasz.g.kostka@gmail.com>
2023-03-15 13:40:35 +0100

    Cache param store

--- src/lambda_saleor_app/settings.py
+++ src/lambda_saleor_app/settings.py
@@ -12,16 +12,21 @@ class Settings(BaseSettings):
     app_id: str
     saleor_domain: str
     ssm_timeout: float = 0.5
-    ssm_secret_prefix: Optional[str]
+
+    ssm_secret_prefix: str
+    # AWS lambda extension env vars
+    parameters_secrets_extension_http_port: int = 2773
 
     ssm_saleor_jwks_key: str = "saleor_jwks"
     ssm_saleor_app_auth_token_key: str = "saleor_app_auth_token"
 
     # Optional - for localstack / local development
     ssm_endpoint: Optional[str]
-    ssm_region: Optional[str]
-    ssm_aws_access_key_id: Optional[str]
-    ssm_aws_secret_access_key: Optional[str]
+    # AWS boto3 env vars
+    aws_access_key_id: Optional[str]
+    aws_secret_access_key: Optional[str]
+    aws_session_token: Optional[str]
+    aws_region: Optional[str]
 
     @validator("ssm_secret_prefix", always=True)
     def set_ssm_secret_prefix(cls, value, values):
--- src/lambda_saleor_app/utils/parameter_store.py
+++ src/lambda_saleor_app/utils/parameter_store.py
@@ -1,4 +1,6 @@
-import botocore
+import urllib.parse
+from urllib.error import URLError
+import json
 import boto3
 from botocore.client import Config
 
@@ -9,13 +11,36 @@ ssm = boto3.client(
     "ssm",
     config=Config(connect_timeout=settings.ssm_timeout, read_timeout=settings.ssm_timeout),
     endpoint_url=settings.ssm_endpoint,
-    aws_access_key_id=settings.ssm_aws_access_key_id,
-    aws_secret_access_key=settings.ssm_aws_secret_access_key,
-    region_name=settings.ssm_region,
+    aws_access_key_id=settings.aws_access_key_id,
+    aws_secret_access_key=settings.aws_secret_access_key,
+    region_name=settings.aws_region,
 )
 
 
+def param_store_cache(key: str, prefix=settings.ssm_secret_prefix) -> str:
+    """
+    Retrieve parameters from AWS Parameter store.
+    In order for this to work on lambda read docs below.
+    https://docs.aws.amazon.com/systems-manager/latest/userguide/ps-integration-lambda-extensions.html
+    """
+    params = urllib.parse.urlencode({
+        'name': f"{prefix}/{key}",
+        'withDecryption': True,
+    })
+    port = settings.parameters_secrets_extension_http_port
+    req = urllib.request.Request(f'http://localhost:{port}/systemsmanager/parameters/get?{params}')
+    req.add_header('X-Aws-Parameters-Secrets-Token', settings.aws_session_token)
+    try:
+        response = json.loads(urllib.request.urlopen(req).read().decode())
+        return response['Parameter']['Value']
+    except URLError:
+        return ""
+
+
 def get_from_ssm(key: str, prefix=settings.ssm_secret_prefix) -> str:
+    value = param_store_cache(key, prefix)
+    if value:
+        return value
     key_with_prefix = f"{prefix}/{key}"
     try:
         parameter = ssm.get_parameter(Name=key_with_prefix, WithDecryption=True)
